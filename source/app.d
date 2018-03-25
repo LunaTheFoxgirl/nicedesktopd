54d5a893124676ebb2a84f9b622df29c5ed897bf
Clipsey <TheisCHR@live.dk>
2018-03-25 04:58:19 +0200

    Initial code commit

---
+++ source/app.d
@@ -0,0 +1,222 @@
+import syscalld : syscall, GETUID;
+import sev.event;
+import jsonizer;
+import ddbus;
+import ddbus.c_lib;
+
+import core.thread;
+import core.time;
+
+import std.stdio;
+import std.conv;
+import std.json;
+
+static import gio.Settings;
+static import std.file;
+
+void main()
+{
+	//Establish connection to DBus.
+	Connection conn = connectToBus();
+
+	//Set up message router.
+	MessageRouter router = new MessageRouter();
+
+	//Setup handler
+	int uid = cast(int)syscall(GETUID);
+	Handlers handler = new Handlers(uid);
+
+	//Register methods
+	registerMethods(router, "/wallpaper", "dk.eclipsingr.nice.wallpaper", handler);
+
+	//Register router and run loop.
+	writeln("<INFO> Starting from UID ", uid, "...");
+
+	registerRouter(conn, router);
+	bool gotem = requestName(conn, "dk.eclipsingr.nice");
+	while (true) {
+		conn.tick();
+	}
+}
+
+class SetWallpaperEventArgs : EventArgs {
+	public string[] wallpapers;
+
+	this(string[] wallpapers) {
+		this.wallpapers = wallpapers;
+	}
+}
+
+class SetWallpaperAdvanceEventArgs : EventArgs {
+	public int time;
+
+	this (int time) {
+		this.time = time;
+	}
+}
+
+class Handlers {
+	private Controller controller;
+
+	this(int uid) {
+		this.controller = new Controller(uid);
+		this.OnNextWallpaper += &controller.NextWallpaper;
+		this.OnPrevWallpaper += &controller.PreviousWallpaper;
+		this.OnSetWallpapers += &controller.SetWallpaperList;
+	}
+
+	public Event OnNextWallpaper = new Event();
+	public Event OnPrevWallpaper = new Event();
+	public Event OnSetWallpapers = new Event();
+	public Event OnSetAdvanceTime = new Event();
+
+	public string next() {
+		OnNextWallpaper(cast(void*)this, null);
+		return current();
+	}
+
+	public string previous() {
+		OnPrevWallpaper(cast(void*)this, null);
+		return current();
+	}
+
+	public string current() {
+		return this.controller.wallpapers[this.controller.current_wallpaper];
+	}
+
+	public string[] list() {
+		return this.controller.wallpapers;
+	}
+
+	public string set_advance_time(int hours, int minutes, int seconds) {
+		int result = (hours*60*60)+(minutes*60)+seconds;
+		try {
+			OnSetAdvanceTime(cast(void*)this, new SetWallpaperAdvanceEventArgs(result));
+			return "success";
+		} catch (Exception ex) {
+			return ex.message.text;
+		}
+	}
+
+	public string set_wallpaper_list(string[] wallpapers) {
+		try {
+			OnSetWallpapers(cast(void*)this, new SetWallpaperEventArgs(wallpapers));
+			return "success";
+		} catch (Exception ex) {
+			return ex.message.text;
+		}
+	}
+}
+
+class Settings {
+	mixin JsonizeMe;
+
+	public @jsonize int timeout;
+	public @jsonize string[] wallpapers;
+}
+
+class Controller {
+	public string[] wallpapers = [];
+	public int current_wallpaper = 0;
+	
+	public int timeout = 5;
+
+	public int user_id;
+	public string home_root;
+
+	private Thread updater_thread;
+	private bool should_kill_thread = false;
+
+	private Connection conn;
+	private PathIface user_iface;
+	private PathIface user_iface_prop;
+
+	this(int uid) {
+		this.updater_thread = new Thread(&UpdateLoop);
+		this.user_id = uid;
+		this.conn = connectToBus(DBusBusType.DBUS_BUS_SYSTEM);
+		this.user_iface = new PathIface(conn, "org.freedesktop.Accounts", "/org/freedesktop/Accounts/User"~this.user_id.text, "org.freedesktop.Accounts.User");
+		this.user_iface_prop = new PathIface(conn, "org.freedesktop.Accounts", "/org/freedesktop/Accounts/User"~this.user_id.text, "org.freedesktop.DBus.Properties");
+		this.ApplySettings();
+
+		this.updater_thread.start();
+	}
+
+	void ApplySettings() {
+		this.home_root = GetHomeDirectory();
+
+		if (!std.file.exists(this.home_root~"/.config/nicewallpaperd.json"))
+		{
+			writeln("<Warn> No settings found in ", this.home_root, "/.config/nicewallpaperd.json!");
+			return;
+		}
+		File f = File(this.home_root~"/.config/nicewallpaperd.json", "r");
+		ubyte[] txt;
+		foreach(ubyte[] t; f.byChunk(4096)) {
+			txt ~= t;
+		}
+		f.close();
+		JSONValue val = parseJSON(cast(string)txt);
+		
+		Settings set = val.fromJSON!Settings();
+		this.timeout = set.timeout;
+		this.wallpapers = set.wallpapers;
+		if (this.wallpapers.length > 0) SystemSetWallpaper(this.wallpapers[0]);
+		writeln("<Info> Settings loaded from ", this.home_root, "/.config/nicewallpaperd.json...");
+	}
+
+	void UpdateLoop() {
+		while (!should_kill_thread) {
+			NextWallpaper(cast(void*)this, null);
+			SystemSetWallpaper(this.wallpapers[this.current_wallpaper]);
+			this.updater_thread.sleep(dur!"seconds"(timeout));
+		}
+	}
+
+	void NextWallpaper(void* sender, EventArgs args) {
+		if (wallpapers.length == 0) return;
+		current_wallpaper++;
+		if (current_wallpaper >= wallpapers.length) current_wallpaper = 0;
+		writeln("<INFO> Set wallpaper to ", current_wallpaper, " @", wallpapers[current_wallpaper], "...");
+	}
+
+	void PreviousWallpaper(void* sender, EventArgs args) {
+		if (wallpapers.length == 0) return;
+		current_wallpaper--;
+		if (current_wallpaper < 0) current_wallpaper = cast(int)wallpapers.length-1;
+		writeln("<INFO> Set wallpaper to ", current_wallpaper, " @", wallpapers[current_wallpaper], "...");
+	}
+
+	void SetWallpaperList(void* sender, EventArgs argz) {
+		SetWallpaperEventArgs args = cast(SetWallpaperEventArgs)argz;
+		this.wallpapers = args.wallpapers;
+		this.current_wallpaper = 0;
+		this.updater_thread = new Thread(&UpdateLoop);
+		this.updater_thread.start();
+	}
+
+	void SetAdvanceTime(void* sender, EventArgs argz) {
+		SetWallpaperAdvanceEventArgs args = cast(SetWallpaperAdvanceEventArgs)argz;
+		this.timeout = args.time;
+	}
+
+	void SystemSetWallpaper(string wallpaper) {
+		// If no wallpapers are present, don't try setting nothing as a wallpaper.
+		if (wallpapers.length == 0) return;
+		this.user_iface.SetBackgroundFile(wallpaper);
+
+		//GIO (GNOME Based) set wallpaper
+		gio.Settings.Settings s = new gio.Settings.Settings("org.gnome.desktop.background");
+		s.setString("picture-uri", wallpaper);
+		s.reset("color-shading-type");
+		if (s.getString("picture-options") == "none") {
+			s.reset("picture-options");
+		}
+		s.apply();
+		s.sync();
+	}
+
+	string GetHomeDirectory() {
+		return this.user_iface_prop.Get("org.freedesktop.Accounts.User", "HomeDirectory").to!string();
+	}
+}
